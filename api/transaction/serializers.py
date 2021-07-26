a5f06de87309974d9296ac6c91f5aa962e0964bf
Kolawole <onaelmangabo@gmail.com>
2021-07-26 22:57:14 +0100

    - wip: add transaction serializer

---
+++ api/transaction/serializers.py
@@ -0,0 +1,12 @@
+from api.transaction.models import Transaction
+from rest_framework import serializers
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    info = serializers.CharField(allow_null=True, allow_blank=True)
+    price = serializers.IntegerField(required=True)
+
+    class Meta:
+        model = Transaction
+        fields = ["id", "product", "price", "qty", "discount", "info"]
+        read_only_field = ["id", "created"]
