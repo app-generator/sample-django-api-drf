9cdaddc9b4af73c82e02231e83b967733f297d1c
Kolawole <onaelmangabo@gmail.com>
2021-07-26 22:54:21 +0100

    - wip: add transaction app to settings

--- api/transaction/apps.py
+++ api/transaction/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class TransactionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transaction'
+    name = 'api.transaction'
+    label = 'api_transaction'
--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "api",
     "api.user",
     "api.authentication",
+    "api.transaction"
 ]
 
 MIDDLEWARE = [
