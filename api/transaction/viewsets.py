9798754d4c9398d9addee406a64dfa754716048e
Kolawole <onaelmangabo@gmail.com>
2021-07-28 22:16:58 +0100

    - wip: add endpoints classes

--- api/transaction/viewsets.py
+++ api/transaction/viewsets.py
@@ -1,8 +1,24 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    pass  # TODO: add get (all), get, edit, create, delete methods
+class TransactionViewSetCreate(viewsets.GenericViewSet, mixins.CreateModelMixin):
+    pass
+
+
+class TransactionViewSetDelete(viewsets.GenericViewSet, mixins.DestroyModelMixin):
+    pass
+
+
+class TransactionViewSetUpdate(viewsets.GenericViewSet, mixins.UpdateModelMixin):
+    pass
+
+
+class TransactionViewSetList(viewsets.GenericViewSet, mixins.ListModelMixin):
+    pass
+
+
+class TransactionViewSetRetrieve(viewsets.GenericViewSet, mixins.RetrieveModelMixin):
+    pass
